1bc5ad07ff34789fa7836ec0d2615cadf7b93b1a
KostaMalsev <kostya.malsev@gmail.com>
2021-01-07 09:57:42 +0200

    cleaning code

--- script.js
+++ script.js
@@ -1,71 +1,17 @@
+//Store hooks and video sizes:
 const video = document.getElementById('webcam');
 const liveView = document.getElementById('liveView');
 const demosSection = document.getElementById('demos');
 const enableWebcamButton = document.getElementById('webcamButton');
-const type_of_model='YOLO';
-//const type_of_model='COCO';
-
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-
 var vidWidth = 0;
 var vidHeight = 0;
-
 var xStart = 0;
 var yStart = 0;
 
-// Check if webcam access is supported.
-function getUserMediaSupported() {
-    return !!(navigator.mediaDevices &&
-        navigator.mediaDevices.getUserMedia);
-}
-
-// If webcam supported, add event listener to button for when user
-// wants to activate it to call enableCam function which we will
-// define in the next step.
-if (getUserMediaSupported()) {
-    enableWebcamButton.addEventListener('click', enableCam);
-} else {
-    console.warn('getUserMedia() is not supported by your browser');
-}
-
-
-var children = [];
-
-
-//Perform prediction based on webcam using Layer model model:
-function predictWebcamTF() {
-    // Now let's start classifying a frame in the stream.
-    detectTFMOBILE(video).then(function () {
-        // Call this function again to keep predicting when the browser is ready.
-        window.requestAnimationFrame(predictWebcamTF);
-    });
-}
-
-
-// Store the resulting model in the global scope of our app.
-var model = undefined;
-tensorLoadModel();
-
-
-
-
-// Enable the live webcam view and start classification.
-function enableCam(event) {
-    // Only continue if the COCO-SSD has finished loading.
-    if (!model) {
-        return;
-    }
-    // Hide the button once clicked.
-    enableWebcamButton.classList.add('removed');
-
-    // getUsermedia parameters to force video but not audio.
-    const constraints = {
-        video: true
-    };
-
-    // Stream video from VAR (for safari also)
-    navigator.mediaDevices.getUserMedia({
+// Stream video (compatible also with safari)
+navigator.mediaDevices.getUserMedia({
         video: {
             facingMode: "environment"
         },
@@ -79,163 +25,95 @@ function enableCam(event) {
             xStart = Math.floor((vw - vidWidth) / 2);
             yStart = (Math.floor((vh - vidHeight) / 2)>=0) ? (Math.floor((vh - vidHeight) / 2)):0;
             $video.play();
-            if(type_of_model=='YOLO')
-            {
-                $video.addEventListener('loadeddata', predictWebcamTF);
-            }else{
-                $video.addEventListener('loadeddata', predictWebcam);
-            }
+            $video.addEventListener('loadeddata', predictWebcamTF);
         }
     });
 }
 
 
 
+var model = undefined;
+model_url = my_model_url;
+//Call load function
+asyncLoadModel(model_url);
 
-/*
-doCORSRequest({
-    method: 'GET',
-    url: 'https://hub.tensorflow.google.cn/tensorflow/ssd_mobilenet_v2/2',
-    data: null
-}, function printResult(result) {
-    model = result;
-    //console.log(result);
-    // Show demo section now model is ready to use.
-    enableWebcamButton.classList.remove('invisible');
-    enableWebcamButton.innerHTML = 'Start camera';
-});
-
-*/
-
-
-/*----------------------------------
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
+//Function Loads the GraphModel type model of
+async function asyncLoadModel(model_url) {
+    model = await tf.loadGraphModel(model_url);
+    console.log('Model loaded');
+}
 
 
 
-<body>
-
-<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"></script>
-<!-- Load the coco-ssd model. -->
-<script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd"></script>
-
-
-<script src="detect.js" type="text/babel"></script>
-
-<img id="img" src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500" crossorigin="anonymous"/>
-
-<script>
-    // Notice there is no 'import' statement. 'cocoSsd' and 'tf' is
-    // available on the index-page because of the script tag above.
-
-    const img = document.getElementById('img');
-
-    // Load the model.
-    //Note: cocoSsd.load() will also work on this without parameter. It will default to the coco ssd model
-    //cocoSsd.load({ modelUrl: 'PATH TO MODEL JSON FILE' }).then(model => {
-    cocoSsd.load().then(model => {
-        // detect objects in the image.
-        model.detect(img).then(predictions => {
-            console.log('Predictions: ', predictions);
-        });
+var children = [];
+//Perform prediction based on webcam using Layer model model:
+function predictWebcamTF() {
+    // Now let's start classifying a frame in the stream.
+    detectTFMOBILE(video).then(function () {
+        // Call this function again to keep predicting when the browser is ready.
+        window.requestAnimationFrame(predictWebcamTF);
     });
-</script>
+}
+const imageSize = 512;
+//Match prob. threshold for object detection:
+var classProbThreshold = 0.4;//40%
+//Image detects object that matches the preset:
+async function detectTFMOBILE(imgToPredict) {
 
-</body>
+    //Get next video frame:
+    await tf.nextFrame();
+    //Create tensor from image:
+    const tfImg = tf.browser.fromPixels(imgToPredict);
+
+    //Create smaller image which fits the detection size
+    const smallImg = tf.image.resizeBilinear(tfImg, [vidHeight,vidWidth]);
 
 
-</html>
-
-
-//YOLO data set:
-export default [
-    'person',
-    'bicycle',
-    'car',
-    'motorbike',
-    'aeroplane',
-    'bus',
-    'train',
-    'truck',
-    'boat',
-    'traffic light',
-    'fire hydrant',
-    'stop sign',
-    'parking meter',
-    'bench',
-    'bird',
-    'cat',
-    'dog',
-    'horse',
-    'sheep',
-    'cow',
-    'elephant',
-    'bear',
-    'zebra',
-    'giraffe',
-    'backpack',
-    'umbrella',
-    'handbag',
-    'tie',
-    'suitcase',
-    'frisbee',
-    'skis',
-    'snowboard',
-    'sports ball',
-    'kite',
-    'baseball bat',
-    'baseball glove',
-    'skateboard',
-    'surfboard',
-    'tennis racket',
-    'bottle',
-    'wine glass',
-    'cup',
-    'fork',
-    'knife',
-    'spoon',
-    'bowl',
-    'banana',
-    'apple',
-    'sandwich',
-    'orange',
-    'broccoli',
-    'carrot',
-    'hot dog',
-    'pizza',
-    'donut',
-    'cake',
-    'chair',
-    'sofa',
-    'pottedplant',
-    'bed',
-    'diningtable',
-    'toilet',
-    'tvmonitor',
-    'laptop',
-    'mouse',
-    'remote',
-    'keyboard',
-    'cell phone',
-    'microwave',
-    'oven',
-    'toaster',
-    'sink',
-    'refrigerator',
-    'book',
-    'clock',
-    'vase',
-    'scissors',
-    'teddy bear',
-    'hair drier',
-    'toothbrush',
-];
+    const resized = tf.cast(smallImg, 'int32');
+    var tf4d_ = tf.tensor4d(Array.from(resized.dataSync()), [1,vidHeight, vidWidth, 3]);
+    const tf4d = tf.cast(tf4d_, 'int32');
+//Perform the detection with your layer model:
+    let predictions = await model.executeAsync(tf4d);
+//Draw box around the detected object:
+    renderPredictionBoxes(predictions[4].dataSync(), predictions[1].dataSync(), predictions[2].dataSync());
+//Dispose of the tensors (so it won't consume memory)
+    tfImg.dispose();
+    smallImg.dispose();
+    resized.dispose();
+    tf4d.dispose();
+}
 
 
 
- */
+//Function Renders boxes around the detections:
+function renderPredictionBoxes (predictionBoxes, predictionClasses, predictionScores)
+{
+    //Remove all detections:
+    for (let i = 0; i < children.length; i++) {
+        liveView.removeChild(children[i]);
+    }
+    children.splice(0);
+//Loop through predictions and draw them to the live view if they have a high confidence score.
+    for (let i = 0; i < 99; i++) {
+//If we are over 66% sure we are sure we classified it right, draw it!
+        const minY = (predictionBoxes[i * 4] * vidHeight+yStart).toFixed(0);
+        const minX = (predictionBoxes[i * 4 + 1] * vidWidth+xStart).toFixed(0);
+        const maxY = (predictionBoxes[i * 4 + 2] * vidHeight+yStart).toFixed(0);
+        const maxX = (predictionBoxes[i * 4 + 3] * vidWidth+xStart).toFixed(0);
+        const score = predictionScores[i * 3] * 100;
+const width_ = (maxX-minX).toFixed(0);
+        const height_ = (maxY-minY).toFixed(0);
+//If confidence is above 70%
+        if (score > 70 && score < 100){
+            const highlighter = document.createElement('div');
+            highlighter.setAttribute('class', 'highlighter');
+            highlighter.style = 'left: ' + minX + 'px; ' +
+                'top: ' + minY + 'px; ' +
+                'width: ' + width_ + 'px; ' +
+                'height: ' + height_ + 'px;';
+            highlighter.innerHTML = '<p>'+Math.round(score) + '% ' + 'Your Object Name'+'</p>';
+            liveView.appendChild(highlighter);
+            children.push(highlighter);
+        }
+    }
+}
